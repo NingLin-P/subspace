7da442dd67984e19760df7b49a63300b9b02dcf4
NingLin-P <ning@subspace.network>
2023-10-13 16:35:55 +0000

    Deploying to gh-pages from @ NingLin-P/subspace@014ee5578952a5462e914b1f995d8db332c8a202 🚀

---
+++ sp_domains/verification/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"enum":["VerificationError"],"fn":["deduplicate_and_shuffle_extrinsics","extrinsics_shuffling_seed","shuffle_extrinsics","verify_invalid_total_rewards_fraud_proof"],"struct":["StorageProofVerifier"]};
