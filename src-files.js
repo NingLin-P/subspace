c05bfaae12e80689dc8250a8ea0e669ab423095a
NingLin-P <ning@subspace.network>
2024-02-19 21:24:08 +0000

    Deploying to gh-pages from @ NingLin-P/subspace@df8ba530c48d5951177350a6cc36831fb4bd183f 🚀

--- src-files.js
+++ src-files.js
@@ -4,7 +4,7 @@ var srcIndex = new Map(JSON.parse('[\
 ["domain_block_preprocessor",["",[],["inherents.rs","lib.rs","stateless_runtime.rs","xdm_verifier.rs"]]],\
 ["domain_client_consensus_relay_chain",["",[],["import_queue.rs","lib.rs"]]],\
 ["domain_client_message_relayer",["",[],["lib.rs","worker.rs"]]],\
-["domain_client_operator",["",[],["aux_schema.rs","bundle_processor.rs","bundle_producer_election_solver.rs","domain_block_processor.rs","domain_bundle_producer.rs","domain_bundle_proposer.rs","domain_worker.rs","domain_worker_starter.rs","fetch_domain_bootstrap_info.rs","fraud_proof.rs","lib.rs","operator.rs","utils.rs"]]],\
+["domain_client_operator",["",[],["aux_schema.rs","bundle_processor.rs","bundle_producer_election_solver.rs","domain_block_processor.rs","domain_bundle_producer.rs","domain_bundle_proposer.rs","domain_worker.rs","fetch_domain_bootstrap_info.rs","fraud_proof.rs","lib.rs","operator.rs","utils.rs"]]],\
 ["domain_eth_service",["",[],["lib.rs","provider.rs","rpc.rs","service.rs"]]],\
 ["domain_pallet_executive",["",[],["lib.rs","weights.rs"]]],\
 ["domain_runtime_primitives",["",[],["lib.rs"]]],\
@@ -16,7 +16,7 @@ var srcIndex = new Map(JSON.parse('[\
 ["orml_vesting",["",[],["lib.rs","weights.rs"]]],\
 ["pallet_block_fees",["",[],["fees.rs","lib.rs"]]],\
 ["pallet_domain_id",["",[],["lib.rs"]]],\
-["pallet_domains",["",[],["block_tree.rs","domain_registry.rs","lib.rs","runtime_registry.rs","staking.rs","staking_epoch.rs","weights.rs"]]],\
+["pallet_domains",["",[],["block_tree.rs","bundle_storage_fund.rs","domain_registry.rs","lib.rs","runtime_registry.rs","staking.rs","staking_epoch.rs","weights.rs"]]],\
 ["pallet_feeds",["",[],["feed_processor.rs","lib.rs"]]],\
 ["pallet_grandpa_finality_verifier",["",[],["chain.rs","grandpa.rs","lib.rs"]]],\
 ["pallet_messenger",["",[],["fees.rs","lib.rs","messages.rs","weights.rs"]]],\
@@ -36,7 +36,7 @@ var srcIndex = new Map(JSON.parse('[\
 ["sp_block_fees",["",[],["lib.rs"]]],\
 ["sp_consensus_subspace",["",[],["digests.rs","inherents.rs","lib.rs","offence.rs"]]],\
 ["sp_domain_digests",["",[],["lib.rs"]]],\
-["sp_domains",["",[],["bundle_producer_election.rs","extrinsics.rs","lib.rs","merkle_tree.rs","proof_provider_and_verifier.rs","storage.rs","valued_trie.rs"]]],\
+["sp_domains",["",[],["bundle_producer_election.rs","core_api.rs","extrinsics.rs","lib.rs","merkle_tree.rs","proof_provider_and_verifier.rs","storage.rs","valued_trie.rs"]]],\
 ["sp_domains_fraud_proof",["",[],["bundle_equivocation.rs","execution_prover.rs","fraud_proof.rs","host_functions.rs","lib.rs","runtime_interface.rs","verification.rs"]]],\
 ["sp_executive",["",[],["lib.rs"]]],\
 ["sp_messenger",["",[],["endpoint.rs","lib.rs","messages.rs"]]],\
@@ -45,11 +45,11 @@ var srcIndex = new Map(JSON.parse('[\
 ["subspace_archiving",["",[["archiver",[],["incremental_record_commitments.rs"]]],["archiver.rs","lib.rs","piece_reconstructor.rs","reconstructor.rs"]]],\
 ["subspace_core_primitives",["",[["crypto",[],["kzg.rs"]],["pieces",[],["serde.rs"]]],["checksum.rs","crypto.rs","lib.rs","objects.rs","pieces.rs","segments.rs","serde.rs"]]],\
 ["subspace_erasure_coding",["",[],["lib.rs"]]],\
-["subspace_farmer",["",[["node_client",[],["node_rpc_client.rs"]],["single_disk_farm",[["farming",[],["rayon_files.rs"]]],["farming.rs","piece_cache.rs","piece_reader.rs","plotting.rs"]],["utils",[],["farmer_piece_getter.rs","piece_validator.rs","readers_and_pieces.rs","ss58.rs"]]],["identity.rs","lib.rs","node_client.rs","piece_cache.rs","reward_signing.rs","single_disk_farm.rs","thread_pool_manager.rs","utils.rs"]]],\
+["subspace_farmer",["",[["node_client",[],["node_rpc_client.rs"]],["single_disk_farm",[["farming",[],["rayon_files.rs"]]],["farming.rs","piece_cache.rs","piece_reader.rs","plotting.rs"]],["utils",[],["farmer_piece_getter.rs","piece_validator.rs","plotted_pieces.rs","ss58.rs"]]],["farmer_cache.rs","identity.rs","lib.rs","node_client.rs","reward_signing.rs","single_disk_farm.rs","thread_pool_manager.rs","utils.rs"]]],\
 ["subspace_farmer_components",["",[],["auditing.rs","file_ext.rs","lib.rs","plotting.rs","proving.rs","reading.rs","sector.rs","segment_reconstruction.rs"]]],\
 ["subspace_malicious_operator",["",[],["chain_spec.rs","lib.rs","malicious_bundle_producer.rs","malicious_bundle_tamper.rs","malicious_domain_instance_starter.rs"]]],\
 ["subspace_metrics",["",[],["lib.rs"]]],\
-["subspace_networking",["",[["behavior",[],["persistent_parameters.rs"]],["constructor",[],["temporary_bans.rs","transport.rs"]],["protocols",[["request_response",[["handlers",[],["generic_request_handler.rs","piece_by_index.rs","segment_header.rs"]]],["handlers.rs","request_response_factory.rs"]],["reserved_peers",[],["handler.rs"]]],["autonat_wrapper.rs","request_response.rs","reserved_peers.rs","subspace_connection_limits.rs"]],["utils",[["rate_limiter",[],["resizable_semaphore.rs"]]],["multihash.rs","piece_provider.rs","rate_limiter.rs","unique_record_binary_heap.rs"]]],["behavior.rs","constructor.rs","lib.rs","node.rs","node_runner.rs","protocols.rs","shared.rs","utils.rs"]]],\
+["subspace_networking",["",[["behavior",[],["persistent_parameters.rs"]],["constructor",[],["temporary_bans.rs","transport.rs"]],["protocols",[["request_response",[["handlers",[],["generic_request_handler.rs","piece_by_index.rs","segment_header.rs"]]],["handlers.rs","request_response_factory.rs"]],["reserved_peers",[],["handler.rs"]]],["autonat_wrapper.rs","request_response.rs","reserved_peers.rs","subspace_connection_limits.rs"]],["utils",[],["multihash.rs","piece_provider.rs","rate_limiter.rs","unique_record_binary_heap.rs"]]],["behavior.rs","constructor.rs","lib.rs","node.rs","node_runner.rs","protocols.rs","shared.rs","utils.rs"]]],\
 ["subspace_proof_of_space",["",[["chiapos",[["table",[],["types.rs"]]],["constants.rs","table.rs","tables.rs","utils.rs"]]],["chia.rs","chiapos.rs","lib.rs","shim.rs"]]],\
 ["subspace_proof_of_time",["",[["aes",[],["x86_64.rs"]]],["aes.rs","lib.rs"]]],\
 ["subspace_rpc_primitives",["",[],["lib.rs"]]],\
